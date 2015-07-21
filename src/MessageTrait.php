df322f4b8ed31fb78257a893d70888fe71d69f37
w00w00 <ahisdumb@gmail.com>
2015-07-21 19:44:01 +0200

    Initial commit

---
+++ src/MessageTrait.php
@@ -0,0 +1,274 @@
+<?php
+
+trait MessageTrait
+{
+    protected $version = '1.1';
+    protected $headers = array();
+    protected $stream;
+
+    /**
+     * Retrieves the HTTP protocol version as a string.
+     *
+     * The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
+     *
+     * @return string HTTP protocol version.
+     */
+    public function getProtocolVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Return an instance with the specified HTTP protocol version.
+     *
+     * The version string MUST contain only the HTTP version number (e.g.,
+     * "1.1", "1.0").
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new protocol version.
+     *
+     * @param string $version HTTP protocol version
+     * @return self
+     */
+    public function withProtocolVersion($version)
+    {
+        if ($version === $this->version) {
+            return $this;
+        }
+
+        $newInstance = clone $this;
+        $newInstance->version = $version;
+        return $newInstance;
+    }
+
+    /**
+     * Retrieves all message header values.
+     *
+     * The keys represent the header name as it will be sent over the wire, and
+     * each value is an array of strings associated with the header.
+     *
+     *     // Represent the headers as a string
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         echo $name . ": " . implode(", ", $values);
+     *     }
+     *
+     *     // Emit headers iteratively:
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         foreach ($values as $value) {
+     *             header(sprintf('%s: %s', $name, $value), false);
+     *         }
+     *     }
+     *
+     * While header names are not case-sensitive, getHeaders() will preserve the
+     * exact case in which headers were originally specified.
+     *
+     * @return array Returns an associative array of the message's headers. Each
+     *     key MUST be a header name, and each value MUST be an array of strings
+     *     for that header.
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Checks if a header exists by the given case-insensitive name.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return bool Returns true if any header names match the given header
+     *     name using a case-insensitive string comparison. Returns false if
+     *     no matching header name is found in the message.
+     */
+    public function hasHeader($name)
+    {
+        $name = trim(strtolower($name));
+
+        $headers = $this->getHeader($name);
+        return (count($headers) > 0);
+    }
+
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * This method returns an array of all the header values of the given
+     * case-insensitive header name.
+     *
+     * If the header does not appear in the message, this method MUST return an
+     * empty array.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string[] An array of string values as provided for the given
+     *    header. If the header does not appear in the message, this method MUST
+     *    return an empty array.
+     */
+    public function getHeader($name)
+    {
+        $name = trim(strtolower($name));
+        if (isset ($this->headers[$name])) {
+            return $this->headers[$name];
+        }
+
+        return [];
+    }
+
+    /**
+     * Retrieves a comma-separated string of the values for a single header.
+     *
+     * This method returns all of the header values of the given
+     * case-insensitive header name as a string concatenated together using
+     * a comma.
+     *
+     * NOTE: Not all header values may be appropriately represented using
+     * comma concatenation. For such headers, use getHeader() instead
+     * and supply your own delimiter when concatenating.
+     *
+     * If the header does not appear in the message, this method MUST return
+     * an empty string.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string A string of values as provided for the given header
+     *    concatenated together using a comma. If the header does not appear in
+     *    the message, this method MUST return an empty string.
+     */
+    public function getHeaderLine($name)
+    {
+        $name = trim(strtolower($name));
+
+        if (!isset ($this->headers[$name]))
+            return "";
+
+        return implode(',', $this->headers[$name]);
+    }
+
+    /**
+     * Return an instance with the provided value replacing the specified header.
+     *
+     * While header names are case-insensitive, the casing of the header will
+     * be preserved by this function, and returned from getHeaders().
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new and/or updated header and value.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @param string|string[] $value Header value(s).
+     * @return self
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
+    public function withHeader($name, $value)
+    {
+        $value = !is_array($value) ? [$value] : $value;
+
+        $newInstance = clone $this;
+        $newInstance->headers[$name] = $value;
+
+        return $newInstance;
+    }
+
+    /**
+     * Return an instance with the specified header appended with the given value.
+     *
+     * Existing values for the specified header will be maintained. The new
+     * value(s) will be appended to the existing list. If the header did not
+     * exist previously, it will be added.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new header and/or value.
+     *
+     * @param string $name Case-insensitive header field name to add.
+     * @param string|string[] $value Header value(s).
+     * @return self
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
+    public function withAddedHeader($name, $value)
+    {
+        $name = trim(strtolower($name));
+        $value = !is_array($value) ? [$value] : $value;
+        $newInstance = clone $this;
+
+        if (isset ($newInstance->headers[$name])) {
+            $newInstance->headers[$name] = array_merge($newInstance->headers[$name], $value);
+        } else {
+            $newInstance->headers[$name] = $value;
+        }
+
+        return $newInstance;
+    }
+
+    /**
+     * Return an instance without the specified header.
+     *
+     * Header resolution MUST be done without case-sensitivity.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that removes
+     * the named header.
+     *
+     * @param string $name Case-insensitive header field name to remove.
+     * @return self
+     */
+    public function withoutHeader($name)
+    {
+        $name = trim(strtolower($name));
+        $newInstance = clone $this;
+
+        unset ($newInstance->headers[$name]);
+        return $newInstance;
+    }
+
+    /**
+     * Gets the body of the message.
+     *
+     * @return StreamInterface Returns the body as a stream.
+     */
+    public function getBody()
+    {
+        return $this->stream;
+    }
+
+    /**
+     * Return an instance with the specified message body.
+     *
+     * The body MUST be a StreamInterface object.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return a new instance that has the
+     * new body stream.
+     *
+     * @param StreamInterface $body Body.
+     * @return self
+     * @throws \InvalidArgumentException When the body is not valid.
+     */
+    public function withBody(StreamInterface $body)
+    {
+        $newInstance = clone $this;
+        $newInstance->stream = $body;
+
+        return $newInstance;
+    }
+
+    protected function setHeaders ( array $headers )
+    {
+        $this->headers = array ( );
+
+        foreach ( $headers as $name => $value )
+        {
+            $name = trim ( strtolower ( $name ) );
+
+            $value = !is_array ( $value ) ? [$value] : $value;
+
+            if ( isset ( $this->headers[$name] ) )
+            {
+                $this->headers[$name] = array_merge ( $this->headers[$name], $value );
+            }
+            else
+            {
+                $this->headers[$name] = $value;
+            }
+        }
+    }
+}
+
+?>
---
+++ src/Stream.php
@@ -0,0 +1,303 @@
+<?php namespace Annexus\Http;
+
+use Psr\Http\Message\StreamInterface;
+
+class Stream implements StreamInterface
+{
+    protected $stream;
+
+    /** @var array Hash of readable and writable stream types */
+    protected $readWriteHash = [
+        'read' => [
+            'r' => true, 'w+' => true, 'r+' => true, 'x+' => true, 'c+' => true,
+            'rb' => true, 'w+b' => true, 'r+b' => true, 'x+b' => true,
+            'c+b' => true, 'rt' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a+' => true
+        ],
+        'write' => [
+            'w' => true, 'w+' => true, 'rw' => true, 'r+' => true, 'x+' => true,
+            'c+' => true, 'wb' => true, 'w+b' => true, 'r+b' => true,
+            'x+b' => true, 'c+b' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a' => true, 'a+' => true
+        ]
+    ];
+
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    /**
+     * Reads all data from the stream into a string, from the beginning to end.
+     *
+     * This method MUST attempt to seek to the beginning of the stream before
+     * reading data and read the stream until the end is reached.
+     *
+     * Warning: This could attempt to load a large amount of data into memory.
+     *
+     * This method MUST NOT raise an exception in order to conform with PHP's
+     * string casting operations.
+     *
+     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
+     * @return string
+     */
+    public function __toString()
+    {
+        try
+        {
+            $this->seek(0);
+            return (string) stream_get_contents ( $this->stream );
+        }
+        catch ( \Exception $e )
+        {
+            return '';
+        }
+    }
+
+    /**
+     * Closes the stream and any underlying resources.
+     *
+     * @return void
+     */
+    public function close()
+    {
+        if ( isset ( $this->stream ) )
+        {
+            if ( is_resource ( $this->stream ) )
+            {
+                fclose ( $this->stream );
+            }
+
+            $this->detach ( );
+        }
+    }
+
+    /**
+     * Separates any underlying resources from the stream.
+     *
+     * After the stream has been detached, the stream is in an unusable state.
+     *
+     * @return resource|null Underlying PHP stream, if any
+     */
+    public function detach()
+    {
+        if ( !isset ( $this->stream ) )
+        {
+            return null;
+        }
+
+        $result = $this->stream;
+        unset ( $this->stream );
+
+        return $result;
+    }
+
+    /**
+     * Get the size of the stream if known.
+     *
+     * @return int|null Returns the size in bytes if known, or null if unknown.
+     */
+    public function getSize()
+    {
+        if ( !isset ( $this->stream ) )
+        {
+            return null;
+        }
+
+        $stats = fstat ( $this->stream );
+
+        return ( isset($stats['size']) ? $stats['size'] : null );
+    }
+
+    /**
+     * Returns the current position of the file read/write pointer
+     *
+     * @return int Position of the file pointer
+     * @throws \RuntimeException on error.
+     */
+    public function tell()
+    {
+        $result = ftell ( $this->stream );
+
+        if ( $result === false )
+        {
+            throw new \RuntimeException('Unable to determine stream position');
+        }
+
+        return $result;    }
+
+    /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof()
+    {
+        return !$this->stream || feof ( $this->stream );
+    }
+
+    /**
+     * Returns whether or not the stream is seekable.
+     *
+     * @return bool
+     */
+    public function isSeekable()
+    {
+        if ( !$this->stream )
+        {
+            return false;
+        }
+
+        $meta = stream_get_meta_data ( $this->stream );
+        return $meta['seekable'];
+    }
+
+    /**
+     * Seek to a position in the stream.
+     *
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @param int $offset Stream offset
+     * @param int $whence Specifies how the cursor position will be calculated
+     *     based on the seek offset. Valid values are identical to the built-in
+     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
+     *     offset bytes SEEK_CUR: Set position to current location plus offset
+     *     SEEK_END: Set position to end-of-stream plus offset.
+     * @throws \RuntimeException on failure.
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        if ( !$this->isSeekable() )
+        {
+            throw new \RuntimeException('Stream is not seekable');
+        }
+        elseif ( fseek ( $this->stream, $offset, $whence ) === -1 )
+        {
+            throw new \RuntimeException('Unable to seek to stream position ' . $offset . ' with whence ' . print_r($whence, true) );
+        }
+    }
+
+    /**
+     * Seek to the beginning of the stream.
+     *
+     * If the stream is not seekable, this method will raise an exception;
+     * otherwise, it will perform a seek(0).
+     *
+     * @see seek()
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @throws \RuntimeException on failure.
+     */
+    public function rewind()
+    {
+        $this->seek(0);
+    }
+
+    /**
+     * Returns whether or not the stream is writable.
+     *
+     * @return bool
+     */
+    public function isWritable()
+    {
+        $meta = stream_get_meta_data ( $this->stream );
+        return isset ( $this->readWriteHash['write'][$meta['mode']] );
+    }
+
+    /**
+     * Write data to the stream.
+     *
+     * @param string $string The string that is to be written.
+     * @return int Returns the number of bytes written to the stream.
+     * @throws \RuntimeException on failure.
+     */
+    public function write($string)
+    {
+        if ( !$this->isWritable() )
+        {
+            throw new \RuntimeException('Cannot write to a non-writable stream');
+        }
+
+        $result = fwrite ( $this->stream, $string );
+        if ( $result === false )
+        {
+            throw new \RuntimeException('Unable to write to stream');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns whether or not the stream is readable.
+     *
+     * @return bool
+     */
+    public function isReadable()
+    {
+        $meta = stream_get_meta_data ( $this->stream );
+        return isset ( $this->readWriteHash['read'][$meta['mode']] );
+    }
+
+    /**
+     * Read data from the stream.
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *     them. Fewer than $length bytes may be returned if underlying stream
+     *     call returns fewer bytes.
+     * @return string Returns the data read from the stream, or an empty string
+     *     if no bytes are available.
+     * @throws \RuntimeException if an error occurs.
+     */
+    public function read($length)
+    {
+        if ( !$this->isReadable ( ) )
+        {
+            throw new \RuntimeException('Cannot read from non-readable stream');
+        }
+
+        return fread ( $this->stream, $length );
+    }
+
+    /**
+     * Returns the remaining contents in a string
+     *
+     * @return string
+     * @throws \RuntimeException if unable to read or an error occurs while
+     *     reading.
+     */
+    public function getContents()
+    {
+        $contents = stream_get_contents ( $this->stream );
+
+        if ( $contents === false )
+        {
+            throw new \RuntimeException('Unable to read stream contents');
+        }
+
+        return $contents;
+    }
+
+    /**
+     * Get stream metadata as an associative array or retrieve a specific key.
+     *
+     * The keys returned are identical to the keys returned from PHP's
+     * stream_get_meta_data() function.
+     *
+     * @link http://php.net/manual/en/function.stream-get-meta-data.php
+     * @param string $key Specific metadata to retrieve.
+     * @return array|mixed|null Returns an associative array if no key is
+     *     provided. Returns a specific key value if a key is provided and the
+     *     value is found, or null if the key is not found.
+     */
+    public function getMetadata($key = null)
+    {
+        if ( !isset ( $this->stream ) )
+        {
+            return null;
+        }
+
+        $meta = stream_get_meta_data ( $this->stream );
+
+        return isset ( $meta[$key] ) ? $meta[$key] : null;
+    }
+}
+
+?>
